912397b6c8e0391b655d322d41f5139f0aa7ac0d
rkohli <ravtejk@gmail.com>
2023-06-28 08:15:14 -0400

    final SwitchPlayer logic and styling

--- src/components/SwitchPlayer/SwitchPlayer.js
+++ src/components/SwitchPlayer/SwitchPlayer.js
@@ -3,8 +3,8 @@ import './SwitchPlayer.scss';
 const SwitchPlayer = ({ opposingPlayerName, onBack, onNext }) => {
     return (
         <div className='switch'>
-            <h2 className='switch__text'>Please pass to {opposingPlayerName} and don't look!</h2>
-            <h3 className='switch__text'>{opposingPlayerName}, click "Next" to take your turn</h3>
+            <h2 className='switch__text'>Pass to {opposingPlayerName}!</h2>
+            <h3 className='switch__subtext'>{opposingPlayerName}, click Next to take your turn</h3>
             <div className='buttons'>
                 <button className='back__button' onClick={onBack}>Back</button>
                 <button className='switch__button' onClick={onNext}>Next</button>
