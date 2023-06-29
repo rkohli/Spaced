f5cc6ad48acac0cbe735848cda1cf78fd1ae58fb
rkohli <ravtejk@gmail.com>
2023-06-29 18:06:16 -0400

    fixed warniings

--- src/components/PlayerReference/PlayerReference.js
+++ src/components/PlayerReference/PlayerReference.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import './PlayerReference.scss';
 
 const PlayerReference = ({ 
--- src/pages/PlayerWinScreen/PlayerWinScreen.js
+++ src/pages/PlayerWinScreen/PlayerWinScreen.js
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import './PlayerWinScreen.scss'
 import { useNavigate, useParams } from 'react-router-dom';
 
