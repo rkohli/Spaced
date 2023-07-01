bb05e5a53903155d8f81a1b729397ca8c237514e
rkohli <ravtejk@gmail.com>
2023-06-30 20:23:49 -0400

    Responsive design added

--- src/components/SwitchPlayer/SwitchPlayer.js
+++ src/components/SwitchPlayer/SwitchPlayer.js
@@ -3,7 +3,7 @@ import './SwitchPlayer.scss';
 const SwitchPlayer = ({ opposingPlayerName, onBack, onNext }) => {
     return (
         <div className='switch'>
-            <h2 className='switch__text'>Pass to {opposingPlayerName}!</h2>
+            <h2 className='switch__text'>Pass to {opposingPlayerName}, and <br></br>don't look!</h2>
             <h3 className='switch__subtext'>{opposingPlayerName}, click Next to take your turn</h3>
             <div className='buttons'>
                 <button className='back__button' onClick={onBack}>Back</button>
